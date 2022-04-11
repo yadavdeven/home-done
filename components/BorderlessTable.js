addaf6a78bc18009b3edeba92ea9dd0b481f6ec7
dEVEN <63048475+yadavdeven@users.noreply.github.com>
2022-04-12 01:33:53 +0530

    Update BorderlessTable.js

--- components/BorderlessTable.js
+++ components/BorderlessTable.js
@@ -79,3 +79,73 @@ const styles = StyleSheet.create({
 });
 
 export default BorderlessTable;
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Animated,
+} from 'react-native';
+
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
+
+function Carousel(props) {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const viewableItemsChanged = React.useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  return (
+    <View>
+      <FlatList
+        data={props.data}
+        // horizontal
+        renderItem={(itemData) => {
+          return (
+            <View
+              style={{
+                height: deviceHeight * 0.3,
+                width: deviceWidth * 0.8,
+                backgroundColor: 'orange',
+                borderRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginHorizontal: deviceWidth * 0.1,
+                marginTop: 80,
+              }}>
+              <Text>{itemData.item.text1}</Text>
+              <Text>{itemData.item.text2}</Text>
+            </View>
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator
+        pagingEnabled
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        onViewableItemsChanged={viewableItemsChanged}
+      />
+    </View>
+  );
+}
+
+// function Carousel(props) {
+//   return (
+//     <FlatList
+//       data={props.data}
+//       renderItem={(itemData) => {
+//         <Slide text1={itemData.item.text1} text2={itemData.item.text2} />
+//       }}
+//     />
+//   )
+// }
+
+export default Carousel;
+
